5f648a10f93cfa2db26766496c19acfe39600e73
MaryB <marybmichaels@outlook.com>
2019-04-13 03:57:13 -0500

    please work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,9 +5,7 @@ var db = require("../models");
 // var request = require("../models/apicall");
 
 module.exports = function(app) {
-
   // Get all saved drinks
-=======
   // eslint-disable-next-line no-unused-vars
   // app.get("/api/:input", function(req, res) {
   //   var drinkname = req.params.input;
@@ -92,7 +90,6 @@ module.exports = function(app) {
     });
   });
 
-
   app.post("/login/:id", function(req, res) {
     // code
   });
@@ -113,24 +110,24 @@ module.exports = function(app) {
   app.post("/api/new", function(req, res) {
     console.log("Chirp Data:");
     console.log(req.body);
-=======
-  app.post("/results", function(req, res) {
-    db.formInfo
-      .create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      })
-      .then(function(dbformInfo) {
-        res.json(dbformInfo);
-      });
-  });
 
-  //   app.post("/send", (req, res) => {
-  //     // send SMS
-  //     const from = "13612354508";
-  //     const to = "13216980087";
-  //     const text = "Hello from Nexmo";
+    app.post("/results", function(req, res) {
+      db.formInfo
+        .create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        })
+        .then(function(dbformInfo) {
+          res.json(dbformInfo);
+        });
+    });
+
+    //   app.post("/send", (req, res) => {
+    //     // send SMS
+    //     const from = "13612354508";
+    //     const to = "13216980087";
+    //     const text = "Hello from Nexmo";
 
     var dbQuery = "INSERT INTO formInfo (name, email, password) VALUES (?,?,?)";
 
