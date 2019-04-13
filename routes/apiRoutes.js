c4487e6397df6306223cae2798b70daedf65a0c1
MaryB <marybmichaels@outlook.com>
2019-04-13 03:29:56 -0500

    fixed apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -45,43 +45,35 @@ module.exports = function(app) {
     // code
   });
 
-  // *********************************************************************************
-  // api-routes.js - this file offers a set of routes for displaying and saving data to the db
-  // *********************************************************************************
-  // Routes
-  // =============================================================
-  module.exports = function(app) {
-    // Get all chirps
-    app.get("/api/all", function(req, res) {
-      var dbQuery = "SELECT * FROM formInfo";
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+    var dbQuery = "SELECT * FROM formInfo";
 
-      connection.query(dbQuery, function(err, result) {
+    connection.query(dbQuery, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      res.json(result);
+    });
+  });
+
+  // Add a chirp
+  app.post("/api/new", function(req, res) {
+    console.log("Chirp Data:");
+    console.log(req.body);
+
+    var dbQuery = "INSERT INTO formInfo (name, email, password) VALUES (?,?,?)";
+
+    connection.query(
+      dbQuery,
+      [req.body.name, req.body.email, req.body.password],
+      function(err, result) {
         if (err) {
           throw err;
         }
-        res.json(result);
-      });
-    });
-
-    // Add a chirp
-    app.post("/api/new", function(req, res) {
-      console.log("Chirp Data:");
-      console.log(req.body);
-
-      var dbQuery =
-        "INSERT INTO formInfo (name, email, password) VALUES (?,?,?)";
-
-      connection.query(
-        dbQuery,
-        [req.body.name, req.body.email, req.body.password],
-        function(err, result) {
-          if (err) {
-            throw err;
-          }
-          console.log("Chirp Successfully Saved!");
-          res.end();
-        }
-      );
-    });
-  };
+        console.log("Chirp Successfully Saved!");
+        res.end();
+      }
+    );
+  });
 };
